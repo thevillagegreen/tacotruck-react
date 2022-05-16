d6b94420a6177ad2561bda5dffa4bcf428a1a111
Aidan <aidanpirish@gmail.com>
2022-05-16 16:05:23 -0400

    store rework

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import ClickArea from './components/ClickArea';
+import Store from './components/Store';
+
+type StoreItem = {
+  name: string,
+  display: string,
+  cost: number,
+  owned: number,
+  factor: number,
+}
 
 const Game: React.FC = () => {
   const [count, setCount] = useState<number>(0);
+  const [perSec, setPerSec] = useState<number>(0);
 
   const handleChange = () => {
     setCount(count + 1);
+  };
+
+  const storePurchase = (cost: number) => {
+    // check if item can be afforded
+    console.log(cost);
   };
 
   return (
@@ -17,6 +32,7 @@ const Game: React.FC = () => {
         </h3>
       </div>
       <ClickArea onClick={handleChange} />
+      <Store count={count} storePurchase={storePurchase} />
     </div>
   );
 };
--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import './Store.css';
 
-export default class Store extends React.PureComponent {
-  render() {
-    return (
-      <div className="Store">
-        <h3>Store</h3>
-      </div>
-    );
-  }
+type StoreItem = {
+  name: string,
+  display: string,
+  cost: number,
+  owned: number,
+  factor: number,
 }
+
+const StoreData = [
+  {
+    name: 'stand',
+    display: 'Taco Stand',
+    cost: 10,
+    owned: 0,
+    factor: 1.15,
+  }, {
+    name: 'restaurant',
+    display: 'Taco Restaurant',
+    cost: 50,
+    owned: 0,
+    factor: 1.15,
+  },
+];
+
+type StoreProps = {
+  count: number;
+  storePurchase?: (cost: number) => void;
+}
+
+const Store: React.FC<StoreProps> = ({
+  storePurchase = (cost: number) => {},
+  count = 0,
+}) => {
+  const purchaseStand = () => {
+    storePurchase(StoreData[1].cost);
+  };
+
+  return (
+    <div className="Store">
+      <h3>Store</h3>
+      <button type="button" onClick={purchaseStand}>Buy Taco Stand - 100</button>
+    </div>
+  );
+};
+
+export default Store;
